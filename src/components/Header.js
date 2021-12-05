a96bd6c7364996ac65159b63742767731bf16121
Ishan koradia <koradiaishan335@gmail.com>
2021-12-05 21:38:04 +0530

    useeffect for toggling the burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+
     return (
         <Container>
             <a href="">
@@ -18,13 +21,19 @@ function Header() {
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <CustomMenu />
+                <CustomMenu onClick={() => setBurgerStatus(true)} />
             </RightMenu>
-            <BurgerNav>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() => setBurgerStatus(false)} />
+                </CloseWrapper>
                 <li><a href="#">Existing Inventory</a></li>
                 <li><a href="#">Used Inventory</a></li>
                 <li><a href="#">Cybertruck</a></li>
                 <li><a href="#">Roadster</a></li>            
+                <li><a href="#">Charging</a></li>            
+                <li><a href="#">Powerwall</a></li>            
+                <li><a href="#">Commercial Energy</a></li>            
             </BurgerNav>    
         </Container>
     )
@@ -85,4 +94,26 @@ const BurgerNav = styled.div`
     z-index: 16;
     list-style: none;
     padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.2s;
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0,0,0,.2);
+
+        a {
+            font-weight: 600;
+        }
+    }
+`
+
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer; 
+`
+
+const CloseWrapper = styled.div`
+    display:flex;
+    justify-content: flex-end;
 `
